6159925bb0b4869cbc1ac4c5b0f0c7f9b8b84365
Mario Rosas <mario.rosas@sopristec.com>
2023-10-10 15:42:46 +0200

    Adding utils scripts

---
+++ scripts/custom_plots.py
@@ -0,0 +1,87 @@
+from matplotlib import pyplot as plt
+from matplotlib.gridspec import GridSpec
+import matplotlib.patches as patches
+import matplotlib.colors as mcolors
+from typing import List, Dict, Tuple
+import random
+from skimage import feature, segmentation
+from skimage.color import rgb2gray, rgba2rgb
+
+class custom_grids():
+  """"
+  Function to print images in a personalized grid of images according to a layout provided
+  or a simple arrangment auto calculated according to the number of columns and rows provided
+  it is also possible to add layers of effects to some images as squares, lines, etc.
+  """
+  def __init__(self,
+             imgs: List,
+             rows: int = 1,
+             cols: int = 1,
+             titles: List = None,
+             order: List = None,
+             figsize: Tuple = (10,10),
+             axis: str = None,
+             cmap: str = None,
+             title_size: int = 12,
+             use_grid_spec: bool = True
+             ):
+      self.imgs = imgs
+      self.rows = rows
+      self.cols = cols
+      self.titles = titles
+      self.order = order
+      self.figsize = figsize
+      self.axis = axis
+      self.cmap = cmap
+      self.title_size = title_size
+      self.use_gris_apec = use_grid_spec
+      self.fig = None
+      self.axs = None
+
+      if not self.order:
+        self.order = [[i, [j, j + 1]] for i in range(self.rows) for j in range(self.cols)]
+
+  def __len__(self):
+    return len(self.imgs)
+
+  def show(self):
+    if not self.use_gris_apec:
+      self.fig, self.axs = plt.subplots(self.rows, self.cols, figsize=self.figsize)
+      if self.rows <= 1 and self.cols <= 1:
+        for idx, img in enumerate(self.imgs):
+          self.axs.imshow(img, cmap=self.cmap)
+          if self.axis:
+            self.axs.axis(self.axis)
+          if self.titles:
+            self.axs.set_title(self.titles[idx], fontsize=self.title_size)
+      elif self.rows <= 1 or self.cols <= 1:
+        for idx, img in enumerate(self.imgs):
+          self.axs[idx].imshow(img, cmap=self.cmap)
+          if self.axis:
+            self.axs[idx].axis(self.axis)
+          if self.titles:
+            self.axs[idx].set_title(self.titles[idx], fontsize= self.title_size)
+      else:
+        im_ind = 0
+        for row in range(self.rows):
+          for column in range(self.cols):
+            self.axs[row][column].imshow(self.imgs[im_ind], cmap=self.cmap)
+            if self.axis:
+              self.axs[row][column].axis(self.axis)
+            if self.titles:
+              self.axs[row][column].set_title(self.titles[im_ind], fontsize= self.title_size)
+            im_ind += 1
+    else:
+      self.fig = plt.figure(constrained_layout=True, figsize=self.figsize)
+      gs = GridSpec(self.rows, self.cols, figure=self.fig)
+      for n, (i, j) in enumerate(zip(self.imgs, self.order)):
+        im = self.fig.add_subplot(gs[j[0], j[1][0]:j[1][1]])
+        if self.cmap:
+          im.imshow(i, cmap=self.cmap)
+        else:
+          im.imshow(i)
+        if self.axis:
+          im.axis('off')
+        if self.titles:
+          im.set_title(self.titles[n], fontsize= self.title_size)
+  
